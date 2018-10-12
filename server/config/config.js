7c9ab24734e14002749a7d1a34e12e064068a086
Leonardo <lboyer@tecnointeligente.com>
2018-10-12 11:03:10 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,8 +1,25 @@
-////////////////////////
-///                 ////
-//       PUERTO     ////
-///                 ////
-////////////////////////
-
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+
+    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb://tecnoboy:upernikao2018@ds229373.mlab.com:29373/ee_data';
+}
+process.env.URLDB = urlDB;
