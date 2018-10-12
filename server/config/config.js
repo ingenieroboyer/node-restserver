1152875371d0b93f26e77d25c53d84df435b9dcf
Leonardo <lboyer@tecnointeligente.com>
2018-10-12 11:40:22 -0300

    VAriable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,6 @@ if (process.env.NODE_ENV === 'dev') {
 } else {
 
     urlDB = process.env.MONGO_URI;
-    urlDB = 'mongodb://tecnoboy:upernikao2018@ds229373.mlab.com:29373/ee_data';
+
 }
 process.env.URLDB = urlDB;
